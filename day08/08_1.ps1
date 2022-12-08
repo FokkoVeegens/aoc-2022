be6c829833ffaddf0cb2296fd9feebab94f6003c
Fokko <24793348+FokkoVeegens@users.noreply.github.com>
2022-12-08 13:09:38 +0100

    Day 8 complete 🌳🌳🌳

--- day08/08_1.ps1
+++ day08/08_1.ps1
@@ -1,2 +1,88 @@
 $dir = (Get-Item -Path $MyInvocation.MyCommand.Path).Directory.FullName
 $data = Get-Content -Path "$dir\input.txt"
+
+$ErrorActionPreference = 'Break'
+
+$height = $data.Count
+$width = $data[0].Length
+$numberOfCornerTrees = 4
+
+[int]$visibleTrees = 2 * $height + 2 * $width - $numberOfCornerTrees
+
+for ($row = 1; $row -lt $height - 1; $row++) 
+{
+    for ($col = 1; $col -lt $width - 1; $col++)
+    {
+        $currentTree = $data[$row].Substring($col, 1)
+        $currentTreeIsVisible = $true
+
+        # Visible from the top
+        for ($rowup = $row - 1; $rowup -ge 0; $rowup--) {
+            if ([int]$data[$rowup].Substring($col, 1) -ge $currentTree)
+            {
+                $currentTreeIsVisible = $false
+                break
+            }
+        }
+        if ($currentTreeIsVisible)
+        {
+            $visibleTrees++
+            continue
+        }
+        else 
+        {
+            $currentTreeIsVisible = $true
+        }
+
+        # Visible from the bottom
+        for ($rowdown = $row + 1; $rowdown -lt $height; $rowdown++) {
+            if ([int]$data[$rowdown].Substring($col, 1) -ge $currentTree)
+            {
+                $currentTreeIsVisible = $false
+                break
+            }
+        }
+        if ($currentTreeIsVisible)
+        {
+            $visibleTrees++
+            continue
+        }
+        else 
+        {
+            $currentTreeIsVisible = $true
+        }
+
+        # Visible from the left
+        for ($colleft = $col - 1; $colleft -ge 0; $colleft--) {
+            if ([int]$data[$row].Substring($colleft, 1) -ge $currentTree)
+            {
+                $currentTreeIsVisible = $false
+                break
+            }
+        }
+        if ($currentTreeIsVisible)
+        {
+            $visibleTrees++
+            continue
+        }
+        else 
+        {
+            $currentTreeIsVisible = $true
+        }
+
+        # Visible from the right
+        for ($colright = $col + 1; $colright -lt $width; $colright++) {
+            if ([int]$data[$row].Substring($colright, 1) -ge $currentTree)
+            {
+                $currentTreeIsVisible = $false
+                break
+            }
+        }
+        if ($currentTreeIsVisible)
+        {
+            $visibleTrees++
+            continue
+        }
+    }
+}
+Write-Host $visibleTrees
--- day08/08_2.ps1
+++ day08/08_2.ps1
@@ -1,2 +1,63 @@
 $dir = (Get-Item -Path $MyInvocation.MyCommand.Path).Directory.FullName
 $data = Get-Content -Path "$dir\input.txt"
+
+$ErrorActionPreference = 'Break'
+
+$height = $data.Count
+$width = $data[0].Length
+
+[int]$highestScenicScore = 0
+
+for ($row = 1; $row -lt $height - 1; $row++) 
+{
+    for ($col = 1; $col -lt $width - 1; $col++)
+    {
+        $currentTree = $data[$row].Substring($col, 1)
+
+        # Trees upwards
+        [int]$upwards = 0
+        for ($rowup = $row - 1; $rowup -ge 0; $rowup--) {
+            $upwards++
+            if ([int]$data[$rowup].Substring($col, 1) -ge $currentTree)
+            {
+                break
+            }
+        }
+
+        # Trees downwards
+        [int]$downwards = 0
+        for ($rowdown = $row + 1; $rowdown -lt $height; $rowdown++) {
+            $downwards++
+            if ([int]$data[$rowdown].Substring($col, 1) -ge $currentTree)
+            {
+                break
+            }
+        }
+
+        # Visible from the left
+        [int]$leftwards = 0
+        for ($colleft = $col - 1; $colleft -ge 0; $colleft--) {
+            $leftwards++
+            if ([int]$data[$row].Substring($colleft, 1) -ge $currentTree)
+            {
+                break
+            }
+        }
+
+        # Visible from the right
+        [int]$rightwards = 0
+        for ($colright = $col + 1; $colright -lt $width; $colright++) {
+            $rightwards++
+            if ([int]$data[$row].Substring($colright, 1) -ge $currentTree)
+            {
+                break
+            }
+        }
+        [int]$score = $upwards * $downwards * $leftwards * $rightwards
+        if ($score -gt $highestScenicScore)
+        {
+            $highestScenicScore = $score
+        }
+    }
+}
+Write-Host $highestScenicScore
